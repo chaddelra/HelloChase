0e312dc5780d2c271706af9a13ea87499de9f307
chaddelra <lr.cmdelara@mmdc.mcl.edu.ph>
2024-02-17 02:01:36 +0800

    second commit

--- src/Chuwaw/HelloChuwaw.java
+++ src/Chuwaw/HelloChuwaw.java
@@ -5,6 +5,7 @@ public class HelloChuwaw {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Meepmeep");
+		System.out.println("Hello Roro");
 
 	}
 
